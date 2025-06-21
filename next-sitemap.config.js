23f7f910d69119811e9904a32467539eaafbe88b
ethanvillalovoz <ethan.villalovoz@gmail.com>
2025-06-21 15:40:45 -0700

    Fixing sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,6 @@
 module.exports = {
   siteUrl: 'https://ethanvillalovoz.vercel.app', // or your custom domain
   generateRobotsTxt: true,
+  generateIndexSitemap: true,
+  outDir: "out", // <- REQUIRED when using `next export`
 };
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true }, // If you use next/image
+  trailingSlash: true, // <- REQUIRED for static sitemap URLs to work
 };
 
 export default nextConfig;
