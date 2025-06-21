a98d85b87d8edd8e48c9476be405cebde8e4bff4
ethanvillalovoz <ethan.villalovoz@gmail.com>
2025-06-21 15:49:14 -0700

    I think I fixed sitemap now?

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,4 @@ module.exports = {
   siteUrl: 'https://ethanvillalovoz.vercel.app', // or your custom domain
   generateRobotsTxt: true,
   generateIndexSitemap: true,
-  outDir: "out", // <- REQUIRED when using `next export`
 };
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   images: { unoptimized: true }, // If you use next/image
   trailingSlash: true, // <- REQUIRED for static sitemap URLs to work
 };
