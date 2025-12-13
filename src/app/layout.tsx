495d025dd8688ade829919e91f996126ed2594cf
ethanvilla27 <ethan.villalovoz@gmail.com>
2025-12-13 03:57:44 -0800

    removed Geist font download

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://ethanvillalovoz.vercel.app"),
@@ -63,7 +52,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
+        className={`antialiased flex flex-col min-h-screen`}
         suppressHydrationWarning
       >
         {/* ✅ Person Structured Data */}
